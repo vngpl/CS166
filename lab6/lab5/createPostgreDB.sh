2b019055b619527468e6f94d969d561a9986d34b
Vansh Nagpal <v.ngpl1911@gmail.com>
2025-03-08 23:05:37 -0800

    organized files

--- lab6/lab5/createPostgreDB.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-echo "creating db named ... "$USER"_lab6_DB"
-cs166_createdb $USER'_lab6_DB'
-cs166_db_status
--- lab6/lab5/startPostgreSQL.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-cs166_initdb
-cs166_db_start
-cs166_db_status
-
-
